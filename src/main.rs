18f57e50e2bfe3d22648dd76578f321a4c96e957
Erlangga Yudi Pradana <erlanggayudipradana@gmail.com>
2025-05-06 10:40:25 +0700

    add features for exporting and importing dashboards

---
+++ src/main.rs
@@ -0,0 +1,190 @@
+use std::env;
+use std::fs;
+use std::sync::OnceLock;
+
+static CONFIG: OnceLock<EnvConfig> = OnceLock::new();
+
+#[derive(Debug)]
+struct EnvConfig {
+    grafana_src_host: String,
+    grafana_src_api_key: String,
+    grafana_dst_host: String,
+    grafana_dst_api_key: String,
+}
+
+fn create_dir(dir_path: &str) {
+    if let Ok(exist) = fs::exists(dir_path) {
+        if !exist {
+            fs::create_dir(dir_path).unwrap();
+        }
+    }
+}
+
+fn list_dir(dir_path: &str) -> Vec<std::path::PathBuf> {
+    fs::read_dir(dir_path).unwrap()
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, std::io::Error>>().unwrap()
+}
+
+async fn export_dashboard() {
+    let config = &CONFIG.get().unwrap();
+
+    let current_dir = {
+        let current_dir = env::current_dir().unwrap();
+        current_dir.to_str().unwrap().to_string()
+    };
+
+    let dashboards_dir = format!("{}/dashboards", current_dir);
+    let folders_dir = format!("{}/folders", current_dir);
+
+    create_dir(&dashboards_dir);
+    create_dir(&folders_dir);
+
+    let grafana_src_api_key = format!("Bearer {}", config.grafana_src_api_key);
+    let auth_value = reqwest::header::HeaderValue::from_str(&grafana_src_api_key).unwrap();
+
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(reqwest::header::AUTHORIZATION, auth_value);
+
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .timeout(std::time::Duration::from_secs(5))
+        .build()
+        .unwrap();
+
+    let endpoint = format!("{}/api/search?type=dash-db", config.grafana_src_host);
+
+    let dashboard_uids: Vec<String> = match client.get(&endpoint).send().await {
+        Err(_) => Vec::new(),
+        Ok(res) => {
+            res.json::<serde_json::Value>().await
+                .ok()
+                .and_then(|json| json.as_array().cloned())
+                .unwrap_or_default()
+                .into_iter()
+                .map(|d| d["uid"].as_str().unwrap().to_string())
+                .collect()        
+        },
+    };
+
+    for uid in dashboard_uids {
+        let endpoint = format!("{}/api/dashboards/uid/{}", config.grafana_src_host, uid);
+
+        if let Ok(res) = client.get(&endpoint).send().await {
+            if let Ok(mut json) = res.json::<serde_json::Value>().await {
+                let dashboard_json = json.as_object_mut().unwrap();
+                let folder_uid = dashboard_json["meta"]["folderUid"].as_str().unwrap().to_string();
+                let dashboard = &mut dashboard_json["dashboard"].as_object_mut().unwrap();
+
+                dashboard.remove("id");
+                dashboard_json.remove("meta");
+
+                dashboard_json.insert("folderUid".to_string(), serde_json::Value::String(folder_uid));
+                dashboard_json.insert("overwrite".to_string(), serde_json::Value::Bool(true));
+
+                let file = fs::File::create(format!("{}/{}.json", dashboards_dir, uid)).unwrap();
+                serde_json::to_writer_pretty(file, &dashboard_json).unwrap();
+            }
+        }
+    }
+
+    let endpoint = format!("{}/api/folders", config.grafana_src_host);
+
+    let folder_uids: Vec<String> = match client.get(&endpoint).send().await {
+        Err(_) => Vec::new(),
+        Ok(res) => {
+            res.json::<serde_json::Value>().await
+                .ok()
+                .and_then(|json| json.as_array().cloned())
+                .unwrap_or_default()
+                .into_iter()
+                .map(|d| d["uid"].as_str().unwrap().to_string())
+                .collect()        
+        },
+    };
+
+    for uid in folder_uids {
+        let endpoint = format!("{}/api/folders/{}", config.grafana_src_host, uid);
+
+        if let Ok(res) = client.get(&endpoint).send().await {
+            if let Ok(mut json) = res.json::<serde_json::Value>().await {
+                let folder_json = json.as_object_mut().unwrap();
+
+                folder_json.remove("id");
+                folder_json.insert("overwrite".to_string(), serde_json::Value::Bool(true));
+
+                let file = fs::File::create(format!("{}/{}.json", folders_dir, uid)).unwrap();
+                serde_json::to_writer_pretty(file, &folder_json).unwrap();
+            }
+        }
+    }
+}
+
+async fn import_dashboard() {
+    let config = &CONFIG.get().unwrap();
+
+    let current_dir = {
+        let current_dir = env::current_dir().unwrap();
+        current_dir.to_str().unwrap().to_string()
+    };
+
+    let folders_dir = format!("{}/folders", current_dir);
+    let dashboards_dir = format!("{}/dashboards", current_dir);
+
+    let grafana_dst_api_key = format!("Bearer {}", config.grafana_dst_api_key);
+    let auth_value = reqwest::header::HeaderValue::from_str(&grafana_dst_api_key).unwrap();
+
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(reqwest::header::AUTHORIZATION, auth_value);
+
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .timeout(std::time::Duration::from_secs(5))
+        .build()
+        .unwrap();
+
+    let folders_path = list_dir(&folders_dir);
+
+    for path in folders_path {
+        let file_content = fs::read_to_string(path).unwrap();
+        let json_data: serde_json::Value = serde_json::from_str(&file_content).unwrap();
+
+        let endpoint = format!("{}/api/folders", config.grafana_dst_host);
+        client.post(&endpoint).json(&json_data).send().await.unwrap();
+    }
+
+    let dashboards_path = list_dir(&dashboards_dir);
+
+    for path in dashboards_path {
+        let file_content = fs::read_to_string(path).unwrap();
+        let json_data: serde_json::Value = serde_json::from_str(&file_content).unwrap();
+
+        let endpoint = format!("{}/api/dashboards/db", config.grafana_dst_host);
+        client.post(&endpoint).json(&json_data).send().await.unwrap();
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    CONFIG.set(EnvConfig {
+        grafana_src_host: std::env::var("GRAFANA_SRC_HOST").expect("GRAFANA_SRC_HOST must be set"),
+        grafana_src_api_key: std::env::var("GRAFANA_SRC_API_KEY").expect("GRAFANA_SRC_API_KEY must be set"),
+        grafana_dst_host: std::env::var("GRAFANA_DST_HOST").expect("GRAFANA_DST_HOST must be set"),
+        grafana_dst_api_key: std::env::var("GRAFANA_DST_API_KEY").expect("GRAFANA_DST_API_KEY must be set"),
+    }).unwrap();
+
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <export | import>", args[0]);
+    } else {
+        match args[1].as_str() {
+            "export" => export_dashboard().await,
+            "import" => import_dashboard().await,
+            other => eprintln!("{}: '{}' it's not a valid argument\n\nHere's the available argument:\n- export\n- import", args[0], other),
+        }
+    }
+}
